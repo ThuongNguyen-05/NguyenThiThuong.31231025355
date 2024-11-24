6a51aa9bb67afc4e491ea0c226a4bc18644544a7
31231025355@ueh.edu.vn <31231025355@ueh.edu.vn>
2024-11-24 22:33:58 +0700

    First time commit

--- Session_07.cs
+++ Session_07.cs
@@ -14,7 +14,7 @@ namespace NguyenThiThuong._31231025355
         }
         public static void Exercise01_P18()
         {
-           
+            Console.WriteLine("Toi muon day bai len github");
         }
       
     }
